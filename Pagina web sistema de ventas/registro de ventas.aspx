88a401034add4b018563912c3385040f3aeb06a8
José Alexander Salinas Lobos <alexlobos303@gmail.com>
2021-11-30 11:20:58 -0600

    registro de ventas

---
+++ Pagina web sistema de ventas/registro de ventas.aspx
@@ -0,0 +1,119 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="registro de ventas.aspx.cs" Inherits="Pagina_web_sistema_de_ventas.registro_de_ventaspx" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        <br />
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+        &nbsp;<asp:Image ID="Image2" runat="server" Height="59px" ImageUrl="~/Recursos/Captura de pantalla (824).png" Width="80px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label1" runat="server" Text="REGISTRO DE VENTAS"></asp:Label>
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label10" runat="server" Text="Id de Venta"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox9" runat="server"></asp:TextBox>
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label11" runat="server" Text="Id de Cliente"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox10" runat="server"></asp:TextBox>
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label2" runat="server" Text="Id Usuario"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label3" runat="server" Text="Id Producto"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label4" runat="server" Text="Fecha de Venta"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label5" runat="server" Text="Hora de Venta"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label6" runat="server" Text="Total de Venta"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label7" runat="server" Text="Efectivo"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label8" runat="server" Text="Vuelto"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+    </p>
+    <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label9" runat="server" Text="Cantidad"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        <asp:Button ID="Button1" runat="server" BackColor="#66FFFF" OnClick="Button1_Click" Text="Actualizar" Width="81px" />
+&nbsp;
+        <asp:Button ID="Button2" runat="server" BackColor="#66FFFF" OnClick="Button2_Click" Text="Modificar" Width="81px" />
+&nbsp;
+        <asp:Button ID="Button3" runat="server" BackColor="#66FFFF" OnClick="Button3_Click" Text="Nuevo" Width="81px" />
+&nbsp;
+        <asp:Button ID="Button4" runat="server" BackColor="#66FFFF" OnClick="Button4_Click" style="height: 26px" Text="Cancelar" Width="81px" />
+&nbsp;
+        <asp:Button ID="Button5" runat="server" BackColor="#66FFFF" OnClick="Button5_Click" Text="Eliminar" Width="81px" />
+&nbsp;
+        <asp:Button ID="Button6" runat="server" BackColor="#66FFFF" OnClick="Button6_Click" Text="Guardar" Width="81px" />
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+</asp:Content>
---
+++ Pagina web sistema de ventas/registro de ventas.aspx.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Pagina_web_sistema_de_ventas
+{
+    public partial class registro_de_ventaspx : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            {
+                SqlConnection conexion = new SqlConnection(@"Data Source=LAPTOP-QM112JVD\MSSQLSERVER01;Initial Catalog=Sistema de ventas;Integrated Security=True");
+                string Id_Usuario;
+                Id_Usuario = (TextBox9.Text);
+
+                SqlCommand consulta_comprobar = new SqlCommand("  Select count(*) From Ventas1 WHERE  Id_Venta=@Id_Venta  ", conexion);
+
+                conexion.Open();
+                consulta_comprobar.Parameters.AddWithValue("@Id_Venta", Id_Usuario);
+                int i;
+                i = Convert.ToInt32(consulta_comprobar.ExecuteScalar());
+
+                if (i > 0)
+                {
+                    MessageBox.Show("La venta que quiere registrar, ya se encuentra registrada");
+                    TextBox1.Text = "";
+                    TextBox2.Text = "";
+                    TextBox3.Text = "";
+                    TextBox4.Text = "";
+                    TextBox5.Text = "";
+                    TextBox6.Text = "";
+                    TextBox7.Text = "";
+                    TextBox8.Text = "";
+                    TextBox9.Text = "";
+                    TextBox10.Text = "";
+                }
+                else
+                {
+
+                    string cadenaconsulta;
+
+                    cadenaconsulta = "Insert into Ventas1 (Id_Cliente,Id_Usuario,Id_Producto, Fecha_de_Venta,Hora_Venta,Total_Venta,Efectivo,Vuelto,Cantidad,Id_Venta)values(@Id_Usuario,@Id_Producto,@Fecha_de_Venta,@Hora_Venta,@Total_Venta,@Efectivo,@Vuelto,@Cantidad,@Id_Venta,@Id_Cliente)";
+
+                    SqlCommand consulta_agregar = new SqlCommand(cadenaconsulta, conexion);
+                    consulta_agregar.Parameters.AddWithValue("@Id_Usuario", TextBox1.Text);
+                    consulta_agregar.Parameters.AddWithValue("@Id_Producto", TextBox2.Text);
+                    consulta_agregar.Parameters.AddWithValue("@Fecha_de_Venta", TextBox3.Text);
+                    consulta_agregar.Parameters.AddWithValue("@Hora_Venta", TextBox4.Text);
+                    consulta_agregar.Parameters.AddWithValue("@Total_Venta", TextBox5.Text);
+                    consulta_agregar.Parameters.AddWithValue("@Efectivo", TextBox6.Text);
+                    consulta_agregar.Parameters.AddWithValue("@Vuelto", TextBox7.Text);
+                    consulta_agregar.Parameters.AddWithValue("@Cantidad", TextBox8.Text);
+                    consulta_agregar.Parameters.AddWithValue("@Id_Venta", TextBox9.Text);
+                    consulta_agregar.Parameters.AddWithValue("@Id_Cliente", TextBox10.Text);
+
+
+                    if (i >= 0)
+                    {
+                        try
+                        {
+
+
+
+                            consulta_agregar.ExecuteNonQuery();
+                            conexion.Close();
+
+                            MessageBox.Show("Se Registro la venta correctamente");
+
+                            TextBox1.Text = "";
+                            TextBox2.Text = "";
+                            TextBox3.Text = "";
+                            TextBox4.Text = "";
+                            TextBox5.Text = "";
+                            TextBox6.Text = "";
+                            TextBox7.Text = "";
+                            TextBox8.Text = "";
+                            TextBox9.Text = "";
+                            TextBox10.Text = "";
+
+                        }
+
+
+
+                        catch (SqlException j)
+                        {
+                            MessageBox.Show(j.ToString());
+                        }
+
+
+                    }
+                }
+
+
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=LAPTOP-QM112JVD\MSSQLSERVER01;Initial Catalog=Sistema de ventas;Integrated Security=True");
+            conexion.Open();
+
+            string actualizar = "UPDATE Ventas1 SET Id_Cliente=@Id_Cliente,Id_Usuario=@Id_Usuario,Id_Producto=@Id_Producto, Fecha_de_Venta=@Fecha_de_Venta, Hora_Venta=@Hora_Venta, Total_Venta=@Total_Venta, Efectivo=@Efectivo,Vuelto=@Vuelto,Cantidad=@Cantidad,Id_Venta=@Id_Venta WHERE Id_Venta=@Id_Venta";
+            SqlCommand cmd2 = new SqlCommand(actualizar, conexion);
+
+            cmd2.Parameters.AddWithValue("@Id_Usuario", TextBox1.Text);
+            cmd2.Parameters.AddWithValue("@Id_Producto", TextBox2.Text);
+            cmd2.Parameters.AddWithValue("@Fecha_de_Venta", TextBox3.Text);
+            cmd2.Parameters.AddWithValue("@Hora_Venta", TextBox4.Text);
+            cmd2.Parameters.AddWithValue("@Total_Venta", TextBox5.Text);
+            cmd2.Parameters.AddWithValue("@Efectivo", TextBox6.Text);
+            cmd2.Parameters.AddWithValue("@Vuelto", TextBox7.Text);
+            cmd2.Parameters.AddWithValue("@Cantidad", TextBox8.Text);
+            cmd2.Parameters.AddWithValue("@Id_Venta", TextBox9.Text);
+            cmd2.Parameters.AddWithValue("@Id_Cliente", TextBox10.Text);
+
+            cmd2.ExecuteNonQuery();
+            MessageBox.Show("Los datos fueron actualizados con exito");
+
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+            TextBox7.Text = "";
+            TextBox8.Text = "";
+            TextBox9.Text = "";
+            TextBox10.Text = "";
+
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=LAPTOP-QM112JVD\MSSQLSERVER01;Initial Catalog=Sistema de ventas;Integrated Security=True");
+
+            conexion.Open();
+            string eliminar;
+
+            eliminar = "DELETE FROM Ventas1 WHERE ID_Venta=@ID_Venta";
+
+            SqlCommand cmd = new SqlCommand(eliminar, conexion);
+
+            cmd.Parameters.AddWithValue("@Id_Venta", TextBox9.Text);
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+            MessageBox.Show(" La venta fue eliminada");
+            TextBox9.Text = "";
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Button1.Visible = Visible;
+            Button2.Visible = Visible;
+            Button3.Visible = Visible;
+            Button4.Visible = Visible;
+            Button5.Visible = Visible;
+            Button6.Visible = Visible;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Button3.Visible = false;
+            Button5.Visible = false;
+            Button1.Visible = false;
+            Button2.Visible = false;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Button3.Visible = false;
+            Button6.Visible = false;
+            Button5.Visible = false;
+            Button2.Visible = false;
+        }
+    }
+}
